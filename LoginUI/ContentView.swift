2a3fc2611e29486f5c3017b38706b8f146a4e3f4
Arzu95 <muhammadarzu3432@gmail.com>
2021-02-03 11:23:15 +0700

    Menambahkan

--- LoginUI/ContentView.swift
+++ LoginUI/ContentView.swift
@@ -1,16 +1,64 @@
-//
-//  ContentView.swift
-//  LoginUI
-//
-//  Created by Apple on 02/02/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Image("co_nf")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                HandleView()
+                FormBox()
+            }
+        }
+    }
+}
+
+struct HandleView : View {
+    var body: some View {
+        VStack {
+            Image(systemName: "hand.draw.fill")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .background(Color.black)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(20)
+            
+            Text("Hello Swift").foregroundColor(.white)
+        }
+    }
+}
+
+struct FormBox: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    var body: some View {
+        VStack (alignment: .leading){
+            Text("Username").font(.callout).bold()
+            TextField("Username", text:$username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Text("Password").font(.callout).bold()
+            SecureField("Password", text:$password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+    
+            Button(action: {print("Hello button")}){
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+            // ini design button
             .padding()
+            .background(Color.black)
+            .cornerRadius(10)
+            .foregroundColor(.white)
+        }
+        
+        // ini design vstack
+        .padding(.all, 20)
+        .background(Color.orange)
+        .cornerRadius(10)
     }
 }
 
